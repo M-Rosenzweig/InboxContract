b6e1603ca2988ec7c518ff5e163bbca2ffae7825
Moshe <moisherosenzweig@gmail.com>
2022-06-22 12:46:56 -0400

    mocha testing  basics

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,32 @@
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
 
+
+
+// let car
+
+// beforeEach(() =>  {
+//     // console.log('vibes');
+//     car = new Car();
+// });
+// class Car {
+//   park() {
+//     return "stopped";
+//   }
+
+//   drive() {
+//     return "vroom";
+//   }
+// }
+
+// describe("Car Methods Test", () => {
+//   it("it can park with good vibes returning 'stopped", () => {
+//     assert.equal(car.park(), "stopped");
+//   });
+
+//   it('can vroom drive', () => {
+//     assert.equal(car.drive(), 'vroom');
+//   })
+// });
